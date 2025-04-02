b80818913f2645809cf8db173d0d9f581bffed9a
Nico <nikolasleonard@gmail.com>
2025-04-01 21:55:59 -0300

    segundo commit del archivo

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 
 int main(){
-    printf("hola mundo");
+    int variable_de_algun_tipo=11;
+    int *puntero = &variable_de_algun_tipo;
+    printf("contenido del puntero: %d  ", *puntero);
+    printf("direccion de memoria almacenada por el puntero: %p  ", puntero);
+    printf("direccion de memoria de la variable: %p  ", &variable_de_algun_tipo);
+    printf("direccion de memoria del puntero: %p  ", &puntero);
+    printf("tamaño de memoria utilizado por la variable: %d  ", sizeof(variable_de_algun_tipo));
     return 0;
 }
